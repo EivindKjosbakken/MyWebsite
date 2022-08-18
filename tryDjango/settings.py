24b0bedaac4afc2dbb8e2e99ebd965dae0b4a74f
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2022-08-18 20:43:09 +0100

    changed to degug=False in settings

--- tryDjango/settings.py
+++ tryDjango/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ivrq9o)j8%o8$xy*34nd9pqo6(2k!gh(27fulchsnbel@%0+e@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #TODO endra
+DEBUG = False #TODO endra
 
 ALLOWED_HOSTS = ["eivindkjosbakken.herokuapp.com", "127.0.0.1"]
 
